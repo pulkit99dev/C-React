2c058bd1a3141e5e7c04a964867b7dedd4ffd054
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-21 17:21:46 +0530

    setState in Depth

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,7 +11,29 @@ class CartItem extends React.Component{
     }
 
     increaseQty = () =>{
-        console.log('this', this.state);
+        // console.log('this', this.state);
+        // this.setState({
+        //     qty : this.state.qty + 1
+        // })
+        this.setState((prevState) => {
+           return{
+            qty : prevState.qty + 1,
+           }
+        })
+    }
+    
+
+    decreaseQty = () => {
+        const {qty} = this.state;
+        if(qty === 0){
+            return;
+        }
+        this.setState((prevState) => {
+            return{
+                qty : prevState.qty - 1
+
+            }
+        })
     }
 
     render(){
@@ -28,7 +50,7 @@ class CartItem extends React.Component{
                     <div className= 'cart-item-actions'>
                         {/* Buttons */}
                         <img alt='increase' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/929/929409.svg?token=exp=1611161468~hmac=e9948e3c1a027a9bdfe687f27b1eb861' onClick={this.increaseQty} />
-                        <img alt='decrease' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/929/929430.svg?token=exp=1611161411~hmac=521277cdd86d635b08ee773e483f62d9' />
+                        <img alt='decrease' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/929/929430.svg?token=exp=1611161411~hmac=521277cdd86d635b08ee773e483f62d9' onClick={this.decreaseQty} />
                         <img alt='delete' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/458/458594.svg?token=exp=1611161533~hmac=c80830e9739641767c51306b37fb89ca' />
                     </div>
                 </div>
