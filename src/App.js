6f29b5ec60cbfbdd498cf3cd93d9e652221df0db
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-21 23:52:47 +0530

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CartItem from './CartItem';
 import Cart from './Cart';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Cart</h1>
       <Cart />
     </div>
---
+++ src/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Cart from './Cart';
+
+class Navbar extends React.Component{
+
+    render(){
+        return(
+            <div style={styles.nav}>
+                <div style={styles.cartIconContainer}>
+                    <img style={styles.cartIcon} src='https://www.flaticon.com/svg/vstatic/svg/3515/3515271.svg?token=exp=1611253233~hmac=7743d3c1d4dfeb2dc23e22efea0871c8' alt='Cart-icon' />
+                    <span style={styles.cartCount}>3</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+const styles = {
+    cartIcon : {
+        height : 32,
+        marginRight: 20
+    },
+    nav:{
+        height: 70,
+        background : '#4267b2',
+        display : 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    cartIconContainer: {
+        position: 'relative'
+    },
+    cartCount:{
+        background: 'darkYellow',
+        borderRadius : '50%',
+        padding: '4px 8px',
+        position: 'absolute',
+        right: 0,
+        top: -9
+    }
+};
+
+export default Navbar;
