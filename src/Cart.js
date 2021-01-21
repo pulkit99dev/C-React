fd853b77ba03be1cd1befc59a5b6dc2169e33933
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-21 22:41:55 +0530

    Button functions

--- src/Cart.js
+++ src/Cart.js
@@ -28,12 +28,36 @@ class Cart extends React.Component{
         ]
         }
     }
+
+    handleIncreaseQty = (product) =>{
+            const {products} = this.state;
+            const index = products.indexOf(product);
+            
+            products[index].qty += 1;
+            this.setState({
+                products : products
+            })
+    }
+
+    handleDecreaseQty = (product) =>{
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        {if (products.qty === 0) {
+            return;
+        }
+        products[index].qty -= 1;
+        this.setState({
+            products : products
+        })
+    }
+    }
+
     render(){
         const {products} = this.state;
         return(
             <div className='cart'>
                 {products.map((product) =>{
-                   return <CartItem product={product} key={product.id} />
+                   return <CartItem product={product} key={product.id} increaseQty={this.handleIncreaseQty} decreaseQty={this.handleDecreaseQty} />
                 })}
             </div>
         )
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,33 +2,6 @@ import React from 'react';
 import Cart from './Cart';
 
 class CartItem extends React.Component{
-   
-
-    increaseQty = () =>{
-        // console.log('this', this.state);
-        // this.setState({
-        //     qty : this.state.qty + 1
-        // })
-        this.setState((prevState) => {
-           return{
-            qty : prevState.qty + 1,
-           }
-        })
-    }
-    
-
-    decreaseQty = () => {
-        const {qty} = this.state;
-        if(qty === 0){
-            return;
-        }
-        this.setState((prevState) => {
-            return{
-                qty : prevState.qty - 1
-
-            }
-        })
-    }
 
     render(){
         const {title, price, qty} = this.props.product;
@@ -43,9 +16,17 @@ class CartItem extends React.Component{
                     <div style={ {color :'green'} }>Qty : {qty}</div>
                     <div className= 'cart-item-actions'>
                         {/* Buttons */}
-                        <img alt='increase' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/929/929409.svg?token=exp=1611161468~hmac=e9948e3c1a027a9bdfe687f27b1eb861' onClick={this.increaseQty} />
-                        <img alt='decrease' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/929/929430.svg?token=exp=1611161411~hmac=521277cdd86d635b08ee773e483f62d9' onClick={this.decreaseQty} />
-                        <img alt='delete' className='action-icons' src='https://www.flaticon.com/svg/vstatic/svg/458/458594.svg?token=exp=1611161533~hmac=c80830e9739641767c51306b37fb89ca' />
+                        <img alt='increase' 
+                        className='action-icons' 
+                        src='https://www.flaticon.com/svg/vstatic/svg/929/929409.svg?token=exp=1611161468~hmac=e9948e3c1a027a9bdfe687f27b1eb861' 
+                        onClick={() => this.props.increaseQty(this.props.product)} />
+                        <img alt='decrease' 
+                        className='action-icons' 
+                        src='https://www.flaticon.com/svg/vstatic/svg/929/929430.svg?token=exp=1611161411~hmac=521277cdd86d635b08ee773e483f62d9' 
+                        onClick={() => this.props.decreaseQty(this.props.product)} />
+                        <img alt='delete' 
+                        className='action-icons' 
+                        src='https://www.flaticon.com/svg/vstatic/svg/458/458594.svg?token=exp=1611161533~hmac=c80830e9739641767c51306b37fb89ca' />
                     </div>
                 </div>
             </div>
